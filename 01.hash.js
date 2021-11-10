ba2095d583c2ab6becec4cb8f2cb9eefa421c210
sohanr <mizan.rahman66d@gmail.com>
2021-11-11 00:02:57 +0600

    hashing

--- 01.hash.js
+++ 01.hash.js
@@ -12,3 +12,16 @@ const hash = (input) => createHash('sha256').update(input).digest('hex');
 
 let password = 'sohan';
 
+const hash1 = hash(password)
+
+console.log(hash1);
+
+password = 'sohanr'
+
+const hash2 = hash(password)
+
+console.log(hash2);
+
+const match = hash1 === hash2;
+
+console.log(match ? 'good password' : 'password does not match');
